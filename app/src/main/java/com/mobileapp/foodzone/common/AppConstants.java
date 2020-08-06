6968d7ec9c16626e87fa16ebc72b72fc47697c89
gysr <shashidhar.ramanujam@gmail.com>
2020-08-06 17:44:57 -0400

    common package created and app constants added

---
+++ app/src/main/java/com/mobileapp/foodzone/common/AppConstants.java
@@ -0,0 +1,39 @@
+package com.mobileapp.foodzone.common;
+
+import com.foodzone.R;
+import com.foodzone.model.BreakfastDo;
+import com.foodzone.model.DinnerDo;
+import com.foodzone.model.GroceryDo;
+import com.foodzone.model.LunchDo;
+
+import java.util.ArrayList;
+
+public class AppConstants {
+
+    public static final int[] menuIcons = {R.drawable.help_icon, R.drawable.rate_icon,
+            R.drawable.share_icon, R.drawable.about_icon, R.drawable.logout_icon};
+    public static final String[] menuTitles = {"Need Help?", "About Us", "Logout"};
+
+    public static final int NEED_HELP = 0;
+    public static final int ABOUT_US = 1;
+    public static final int LOGIN_OR_LOGOUT = 2;
+    public static String DATABASE_NAME = "generic.sqlite";
+    public static String DATABASE_PATH = "";
+
+    public static ArrayList<LunchDo> listLunch = new ArrayList<>();
+    public static ArrayList<LunchDo> listCartLunch = new ArrayList<>();
+
+
+    public static ArrayList<DinnerDo> listDinner = new ArrayList<>();
+    public static ArrayList<DinnerDo> listCartDinner = new ArrayList<>();
+
+
+    public static ArrayList<GroceryDo> listGrocery = new ArrayList<>();
+    public static ArrayList<GroceryDo> listCartGrocery = new ArrayList<>();
+
+
+
+    public static ArrayList<BreakfastDo> listBreakfast = new ArrayList<>();
+    public static ArrayList<BreakfastDo> listCartBreakfast = new ArrayList<>();
+
+}
