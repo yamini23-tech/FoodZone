7e980ab407ff5024bebafa32fe94010f35c7698a
Mounika-43 <Mounika-43>
2020-08-06 22:59:51 -0400

    fragments added

---
+++ app/src/main/java/com/mobileapp/foodzone/fragment/MonthYearPickerDialog.java
@@ -0,0 +1,65 @@
+package com.mobileapp.foodzone.fragment;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import com.foodzone.R;
+
+import java.util.Calendar;
+
+/**
+ * Created by sandy on 4/12/2018.
+ */
+public class MonthYearPickerDialog extends DialogFragment {
+
+    private static final int MAX_YEAR = 2099;
+    private DatePickerDialog.OnDateSetListener listener;
+    public static NumberPicker monthPicker,yearPicker;
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        Calendar cal = Calendar.getInstance();
+
+        View dialog = inflater.inflate(R.layout.date, null);
+        monthPicker = (NumberPicker) dialog.findViewById(R.id.monthPicker);
+        yearPicker = (NumberPicker) dialog.findViewById(R.id.yearPicker);
+
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(cal.get(Calendar.MONTH) + 1);
+
+        int year = cal.get(Calendar.YEAR);
+        yearPicker.setMinValue(year);
+        yearPicker.setMaxValue(MAX_YEAR);
+        yearPicker.setValue(year);
+
+        builder.setView(dialog)
+                // Add action buttons
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDateSet(null, yearPicker.getValue(), monthPicker.getValue(), 0);
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MonthYearPickerDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
