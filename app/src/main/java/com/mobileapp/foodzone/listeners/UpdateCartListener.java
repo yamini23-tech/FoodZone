6e47b1e0756b9df904d9554a058bc4428e30fbea
Mounika-43 <Mounika-43>
2020-08-06 23:07:08 -0400

    listners added

---
+++ app/src/main/java/com/mobileapp/foodzone/listeners/UpdateCartListener.java
@@ -0,0 +1,10 @@
+package com.mobileapp.foodzone.listeners;
+
+/**
+ * Created by dalu547 on 3/5/2018.
+ */
+
+public interface UpdateCartListener {
+
+    void updateCartCount();
+}
---
+++ app/src/main/java/com/mobileapp/foodzone/listeners/UpdateTotalPriceListener.java
@@ -0,0 +1,10 @@
+package com.mobileapp.foodzone.listeners;
+
+/**
+ * Created by dalu547 on 3/5/2018.
+ */
+
+public interface UpdateTotalPriceListener {
+
+    void updateTotalPrice();
+}
