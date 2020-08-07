af51ac4f77a3220639ce32bf38ad85830ad2ef5b
Mounika-43 <Mounika-43>
2020-08-06 22:56:33 -0400

    database added

---
+++ app/src/main/java/com/mobileapp/foodzone/database/StorageManager.java
@@ -0,0 +1,79 @@
+package com.mobileapp.foodzone.database;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.foodzone.model.RegisterDO;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by Kishore on 12/12/2018.
+ * Copyright (C) 2018 TBS - All Rights Reserved
+ */
+
+public class StorageManager {
+    private static final String TAG = "StorageManager";
+
+    private static TBSDatabaseHelper sDatabaseHelper;
+    private static StorageManager sStorageManager;
+
+    private StorageManager(Context context) {
+        sDatabaseHelper = new TBSDatabaseHelper(context);
+    }
+
+    public static synchronized StorageManager getInstance(Context context) {
+        if (sStorageManager == null) {
+            sStorageManager = new StorageManager(context);
+        }
+        return sStorageManager;
+    }
+
+  
+    public void saveRegisterDO(Activity activity, RegisterDO registerDO) {
+        try {
+            File file = new File(activity.getApplication().getFilesDir().toString() + "/", "RegisterDO.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(registerDO);
+            os.close();
+            fos.close();
+        } catch (Exception e) {
+            Log.e(TAG, "saveregisterDO() : " + e.getMessage());
+        }
+    }
+
+    public void deleteRegisterDO(Activity activity) {
+        try {
+            File file = new File(activity.getApplication().getFilesDir().toString() + "/", "RegisterDO.txt");
+            if (file.exists())
+                file.delete();
+        } catch (Exception e) {
+            Log.e(TAG, "saveRegisterDO() : " + e.getMessage());
+        }
+    }
+
+    public RegisterDO getRegisterDO(Context context) {
+        try {
+            File file = new File(((Activity) context).getApplication().getFilesDir().toString() + "/", "RegisterDO.txt");
+            if (file.exists()) {
+                FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream is = new ObjectInputStream(fis);
+                RegisterDO RegisterDO = (RegisterDO) is.readObject();
+                is.close();
+                fis.close();
+                return RegisterDO;
+            }
+        } catch (Exception e) {
+            Log.e("StorageManager", "getRegisterDO() : " + e.getMessage());
+            return new RegisterDO();
+        }
+        return new RegisterDO();
+    }
+
+}
