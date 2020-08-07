8a5b0b786e1a9b0072093d1e7140c5d7bb06fadd
Mounika-43 <Mounika-43>
2020-08-06 23:10:35 -0400

    colors added

---
+++ app/src/main/java/com/mobileapp/foodzone/model/CommonDO.java
@@ -0,0 +1,9 @@
+package com.mobileapp.foodzone.model;
+
+import java.io.Serializable;
+
+public class CommonDO implements Serializable {
+    public int status = 0;
+    public String statusMessage = "";
+
+}
